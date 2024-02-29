359ab7ba7f75dad8d1745c3163c6f1f994a0a067
Kamil Marczak <kamil.codemate@gmail.com>
2024-02-29 23:34:03 +0100

    Set up gh-pages, update hero section

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './PageStyles/MainPage.scss';
 const MainPage: React.FC = () => {
   return (
-    <div>
+    <div className='container'>
       <nav className='main-navbar'>
         <div className='navbar-logo'>
           <span className='navbar-logo-first-element'>Erasmus+ 2024 | </span>
@@ -16,14 +16,36 @@ const MainPage: React.FC = () => {
             O projekcie
           </a>
           <a href='/' className='navbar-menu-item'>
-            Galeria
+            Lista uczesników
           </a>
           <a href='/' className='navbar-menu-item'>
-            Lista uczesników
+            Galeria
           </a>
         </div>
       </nav>
-      <section className='hero'></section>
+      <section className='hero'>
+        <div className='main-content'>
+          <div className='main-title'>
+            <span id='hero-first-element'>Praktyki zagraniczne Elektronika</span> <br />
+            <span id='hero-second-element'>Malaga, 2024</span>
+          </div>
+          <div className='hero-menu'>
+            <a href='/' className='hero-menu-item'>
+              O projekcie
+            </a>
+            <a href='/' className='hero-menu-item'>
+              Oczekiwania uczestników
+            </a>
+            <a href='/' className='hero-menu-item'>
+              Przebieg projektu
+            </a>
+
+            <a href='/' className='hero-menu-item'>
+              Galeria i multimedia
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
