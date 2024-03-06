e2a946b6a11d7a38460010c3e1891d7248b32d1d
Michał Mikuła <mikulamichal@proton.me>
2024-03-06 21:23:49 +0100

    Reverse arrows in expectations list

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -109,10 +109,10 @@ Poprzez wsp√≥lne projekty i inicjatywy, Erasmus+ 2024 ma na celu budowanie trwa≈
           </span>
           <div className="description-content">
             <div className='expectations-control'>
-<svg className='left-arrow' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" onClick={MoveLeft} style={{visibility: expectationsListLeft > ((Expectations.length - 3) * 33)*-1 ? "visible" : "hidden"}}>
+<svg className='left-arrow' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" onClick={MoveRight} style={{visibility: expectationsListLeft !== 0 ? "visible" : "hidden"}}>
   <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
 </svg>
-<svg className='right-arrow' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" onClick={MoveRight} style={{visibility: expectationsListLeft !== 0 ? "visible" : "hidden"}}>
+<svg className='right-arrow' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" onClick={MoveLeft} style={{visibility: expectationsListLeft > ((Expectations.length - 3) * 33)*-1 ? "visible" : "hidden"}}>
   <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
 </svg>
             </div>
