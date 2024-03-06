da03bc11993883cbc97317d29debb050585827b9
Michał Mikuła <mikulamichal@proton.me>
2024-03-06 21:10:48 +0100

    Normalise table sizes, add attendees list download

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -203,6 +203,12 @@ Poprzez wspólne projekty i inicjatywy, Erasmus+ 2024 ma na celu budowanie trwa�
         <div className='description attendees' id="lista-uczniow">
           <span className='description-title'>
             Lista uczestników
+            <a href="/files/lista-uczestników.pdf" target="_blank" rel="noreferrer" download>
+              <svg xmlns="http://www.w3.org/2000/svg" className="download" viewBox="0 0 16 16">
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+              </svg>
+            </a>
           </span>
           <div className='description-content'>
                 <table>
@@ -215,7 +221,7 @@ Poprzez wspólne projekty i inicjatywy, Erasmus+ 2024 ma na celu budowanie trwa�
                   </thead>
                   <tbody>
                     {
-                      Expectations.map((expectation, index) => (
+                      Expectations.sort((a,b) => a.author.localeCompare(b.author)).map((expectation, index) => (
                         <tr>
                           <td>{index+1}</td>
                           <td>{expectation.author}</td>
