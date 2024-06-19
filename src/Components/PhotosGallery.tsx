5601ac55a80e28021c8d9a78d6a3ab2e3e9dee13
Michał Mikuła <mikulamichal@proton.me>
2024-06-19 23:50:23 +0200

    Add images display mechanism to photos gallery

--- src/Components/PhotosGallery.tsx
+++ src/Components/PhotosGallery.tsx
@@ -1,36 +1,33 @@
 import { FC, useEffect, useState } from "react";
 import { IPhotosGallery } from "./interfaces/PhotosGallery.interface";
 
+const img_context = require.context("../../public/images", true);
 
 const PhotosGallery: FC<IPhotosGallery> = ({gallery}) => {
 
-    const [images, setImages] = useState<string[]>([]);
-    const [loadError, setLoadError] = useState<boolean>(false);
+    const [images, setImages] = useState<any[]>([]);
 
     useEffect(() => {
-        fetch("http://localhost:3200/api/photos")
-            .then((res) => res.json())
-            .then((res) => {
-                setImages(res);
-            })
-            .catch((err) => {
-                setLoadError(true);
-                console.log(err);
-            })
-    }, [gallery]);
+        console.log(img_context);
+
+        const img_list = img_context.keys().filter((img: string) => img.includes(gallery));
+        console.log(img_list);
+        setImages(img_list.map((img: string) => img_context(img)));
+        console.log(images);
+    }, [gallery, img_context]);
 
     return (
         <>
             {
-                images.length === 0 || loadError ? (
+                images.length === 0 ? (
                     <div className="photos-gallery-error">
-                        {loadError ? "Wystąpił błąd przy ładowaniu zdjęć" : "Brak zdjęć"}
+                        {"Brak zdjęć"}
                     </div>
                 ) : (
                     <div className="photos-gallery-content-container">
                         {
                             images.map((image, index) => (
-                                <img key={index} src={`http://localhost:3200/static/photos/${gallery}/${image}`} alt={image.split("/").at(image.length - 1)} />
+                                <img key={index} src={image} alt={`${gallery}-${index + 1}`} />
                             ))
                         }
                     </div>
